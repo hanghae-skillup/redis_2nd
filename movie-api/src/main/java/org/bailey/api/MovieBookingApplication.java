e3fbbb9ea12ce59399d466ede23971c94d28d9e7
WorkaHolic <baileypark@kakao.com>
2025-03-17 04:37:01 +0900

    feat: initialize movie booking system project

---
+++ movie-api/src/main/java/org/bailey/api/MovieBookingApplication.java
@@ -0,0 +1,11 @@
+package org.bailey.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieBookingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MovieBookingApplication.class, args);
+    }
+}
