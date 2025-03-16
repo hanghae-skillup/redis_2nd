c58843a367a501fb0f467b9f99d19d6e61568cc2
juhyun-leee <dev.juhyun@gmail.com>
2025-03-16 19:00:50 +0900

    feat: api 모듈 코드 작성
     - application.yml
     - Controller
     - DB 스키마.sql (ddl.sql)

---
+++ api/build.gradle.kts
@@ -0,0 +1,7 @@
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter:3.4.3")
+    implementation("org.springframework.boot:spring-boot-starter-web:3.4.3")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    implementation(project(":application"))
+}
---
+++ api/src/main/kotlin/com/hanghe/redis/movie/MovieControllerV1.kt
@@ -0,0 +1,22 @@
+package com.hanghe.redis.movie
+
+import com.hanghe.redis.movie.response.GetMovieScreeningResponses
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/movies")
+class MovieControllerV1(
+    private val service: MovieService
+) {
+
+
+    @GetMapping
+    fun getAllScreeningMovies(): ResponseEntity<GetMovieScreeningResponses> {
+        return ResponseEntity.ok(
+            service.getAllScreeningMovies()
+        )
+    }
+}
