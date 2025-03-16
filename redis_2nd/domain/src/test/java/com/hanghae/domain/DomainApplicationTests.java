d9b8c08fe931e20004c4db022ef1c7d06ede956f
1ee-jennie <jjungeun1244@gmail.com>
2025-03-17 01:05:45 +0900

    init multi-module project

---
+++ redis_2nd/domain/src/test/java/com/hanghae/domain/DomainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
