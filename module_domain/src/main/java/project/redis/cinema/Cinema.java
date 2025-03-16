48cd3872639c2b89fa17a0365b66f44e8002b468
CJ-1998 <choijun0627@konkuk.ac.kr>
2025-03-16 10:12:21 +0900

    Feat : 영화관 좌석 필드 및 로직 추가
    Cinema 클래스에 좌석 필드 추가
    좌석 5*5 생성하는 로직 추가

--- module_domain/src/main/java/project/redis/cinema/Cinema.java
+++ module_domain/src/main/java/project/redis/cinema/Cinema.java
@@ -1,13 +1,44 @@
 package project.redis.cinema;
 
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import project.redis.seat.Seat;
 
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Cinema {
+    public static final int ROWS = 5;
+    public static final int COLUMNS = 5;
+
     private String cinemaName;
+    private List<List<Seat>> seats;
 
     public static Cinema of(String cinemaName) {
-        return new Cinema(cinemaName);
+        List<List<Seat>> seats = createSeats();
+        return new Cinema(cinemaName, seats);
+    }
+
+    private static List<List<Seat>> createSeats() {
+        List<List<Seat>> seats = new ArrayList<>();
+        for (int row = 0; row < ROWS; row++) {
+            List<Seat> seat = new ArrayList<>();
+            for (int col = 0; col < COLUMNS; col++) {
+                Seat oneSeat = createOneSeat(row, col);
+                seat.add(oneSeat);
+            }
+            seats.add(seat);
+        }
+        return seats;
+    }
+
+    private static Seat createOneSeat(int row, int col) {
+        String seatRow = getSeatRow(row);
+        Integer setColumn = col + 1;
+        return Seat.of(false, seatRow, setColumn);
+    }
+
+    private static String getSeatRow(int rowNumber) {
+        return String.valueOf((char) ('A' + rowNumber));
     }
 }
