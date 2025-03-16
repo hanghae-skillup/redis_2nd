670f246aed384899b7a2480d4cc1e468c2883d3a
skdev0619 <skdev0619@naver.com>
2025-03-16 17:35:06 +0900

    02-application 모듈 추가

---
+++ 02-application/src/main/java/com/hanghae/HanghaeServiceApplication.java
@@ -0,0 +1,11 @@
+package com.hanghae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaeServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HanghaeServiceApplication.class, args);
+    }
+}
