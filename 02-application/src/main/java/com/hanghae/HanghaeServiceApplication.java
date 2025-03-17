aa2b6c451924b935594d12b2aef7184164c84957
skdev0619 <skdev0619@naver.com>
2025-03-17 18:08:00 +0900

    엔드포인트가 아닌 부분에 프로필 적용

--- 02-application/src/main/java/com/hanghae/HanghaeServiceApplication.java
+++ 02-application/src/main/java/com/hanghae/HanghaeServiceApplication.java
@@ -2,7 +2,9 @@ package com.hanghae;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
+@Profile("application")
 @SpringBootApplication
 public class HanghaeServiceApplication {
     public static void main(String[] args) {
