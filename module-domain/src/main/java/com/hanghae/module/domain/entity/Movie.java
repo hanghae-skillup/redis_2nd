530454c4d77f756494658628156213ce701d4bf5
jaeseung choun <jaeseung_choun@tmax.co.kr>
2025-03-18 17:05:26 +0900

    feat: 엔티티 구성

--- module-domain/src/main/java/com/hanghae/module/domain/entity/Movie.java
+++ module-domain/src/main/java/com/hanghae/module/domain/entity/Movie.java
@@ -1,4 +1,38 @@
 package com.hanghae.module.domain.entity;
 
+import com.hanghae.module.domain.enums.Genre;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "movie")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Movie {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String title;
+
+  @Column(nullable = false)
+  private String rating;
+
+  @Column(nullable = false)
+  private LocalDate releaseDate;
+
+  @Column(nullable = false)
+  private String thumbnailUrl;
+
+  @Column(nullable = false)
+  private Integer runningTime;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private Genre genre;
 }
