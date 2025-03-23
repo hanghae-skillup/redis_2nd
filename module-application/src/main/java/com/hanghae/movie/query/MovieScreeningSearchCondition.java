55448e108d206ca824bf9e54b1623ca73f6bff92
skdev0619 <skdev0619@naver.com>
2025-03-23 18:49:22 +0900

    파라메터 유효성 체크 변경

--- module-application/src/main/java/com/hanghae/movie/query/MovieScreeningSearchCondition.java
+++ module-application/src/main/java/com/hanghae/movie/query/MovieScreeningSearchCondition.java
@@ -4,11 +4,12 @@ import com.hanghae.SearchCriteria;
 import com.hanghae.common.enums.SearchMatchType;
 import com.hanghae.movie.MovieGenre;
 import com.hanghae.movie.MovieStatus;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
@@ -16,26 +17,30 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 public class MovieScreeningSearchCondition {
-    @NotNull
+    //private static final int MOVIE_TITLE_SIZE = 255;
+
     private SearchCriteria<String> movieName;
-    @NotNull
     private List<MovieGenre> genre;
     private List<MovieStatus> status;
     private boolean isReleaseDateAsc;
 
-    public  MovieScreeningSearchCondition(SearchCriteria<String> movieName, List<MovieGenre> genre, List<MovieStatus> status, boolean isReleaseDateAsc) {
+    public MovieScreeningSearchCondition(SearchCriteria<String> movieName, List<MovieGenre> genre, List<MovieStatus> status, boolean isReleaseDateAsc) {
         this.movieName = movieName;
         this.genre = genre;
         this.status = status;
         this.isReleaseDateAsc = isReleaseDateAsc;
     }
 
-    public String getMovieNameValue(){
+    /*@AssertTrue
+    public boolean isValidTitleSize() {
+        return movieName.getValue().length() <= MOVIE_TITLE_SIZE;
+    }*/
+
+    public String getMovieNameValue() {
         return movieName.getValue();
     }
 
-    public SearchMatchType getMovieNameSearchType(){
+    public SearchMatchType getMovieNameSearchType() {
         return movieName.getType();
     }
 }
-
