a7a21abe1a9163353b1b1bc47e2c2ab78fff7ff9
CJ-1998 <choijun0627@konkuk.ac.kr>
2025-03-29 23:37:35 +0900

    Feat : SeatEntity에서 Theater 삭제
    SeatEntity에 존재하는 Theater 필드는 TheaterEntity를 위한 것
    TheaterEntity에 List<SeatEntity> 삭제했기에 삭제

--- module_infrastructure/src/main/java/project/redis/seat/entity/SeatEntity.java
+++ module_infrastructure/src/main/java/project/redis/seat/entity/SeatEntity.java
@@ -5,8 +5,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -14,7 +12,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import project.redis.common.entity.BaseEntity;
-import project.redis.theater.entity.TheaterEntity;
 
 @Entity
 @Table(name = "seat")
@@ -36,9 +33,4 @@ public class SeatEntity extends BaseEntity {
 
     @Column(nullable = false)
     private Integer seatColumn;
-
-    // TODO : Theater에 좌석 필요 없다는 것을 깨달아서 차근차근 삭제할 것
-    @ManyToOne
-    @JoinColumn(name = "theater_id")
-    private TheaterEntity theater;
 }
