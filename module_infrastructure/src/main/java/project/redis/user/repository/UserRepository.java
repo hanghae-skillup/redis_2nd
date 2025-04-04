11b160ba621f0a67d141db5d8c0691861d6fe6e7
CJ-1998 <choijun0627@konkuk.ac.kr>
2025-04-04 22:53:26 +0900

    Refactor : 불필요한 annotation 제거
    JpaRepository에 @Repository annotation 제거

--- module_infrastructure/src/main/java/project/redis/user/repository/UserRepository.java
+++ module_infrastructure/src/main/java/project/redis/user/repository/UserRepository.java
@@ -1,9 +1,7 @@
 package project.redis.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import project.redis.user.entity.UserEntity;
 
-@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 }
