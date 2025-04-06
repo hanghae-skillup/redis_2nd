5282d0545ed9f9bcc8175cf508e39a02872e18db
CJ-1998 <choijun0627@konkuk.ac.kr>
2025-04-06 21:04:34 +0900

    Feat : 예약 api의 Rate Limit 위한 Ascpect 구현

---
+++ module_infrastructure/src/main/java/project/redis/ratelimiter/reserveratelimiter/ReserveRateLimiterAspect.java
@@ -0,0 +1,24 @@
+package project.redis.ratelimiter.reserveratelimiter;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class ReserveRateLimiterAspect {
+
+    private final ReserveRateLimiter reserveRateLimiter;
+
+    public ReserveRateLimiterAspect(@Qualifier("GuavaReserveRateLimiter") ReserveRateLimiter reserveRateLimiter) {
+        this.reserveRateLimiter = reserveRateLimiter;
+    }
+
+    @Around("@annotation(limitReservationPerTime)")
+    public Object setRateLimiter(ProceedingJoinPoint joinPoint, LimitReservationPerTime limitReservationPerTime)
+            throws Throwable {
+        return reserveRateLimiter.tryApiCall(limitReservationPerTime, joinPoint);
+    }
+}
