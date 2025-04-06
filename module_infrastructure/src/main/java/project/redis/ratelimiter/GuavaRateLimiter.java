d991f5a821c18ea49ce2cc079c886ab795d629b5
CJ-1998 <choijun0627@konkuk.ac.kr>
2025-04-06 16:47:39 +0900

    Feat : Guava 사용 RateLimiter 구현체 구현 중
    Guava 사용해 RateLimiter 구현할 구현체 클래스 생성
    로직 작성 중

---
+++ module_infrastructure/src/main/java/project/redis/ratelimiter/GuavaRateLimiter.java
@@ -0,0 +1,50 @@
+package project.redis.ratelimiter;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.springframework.stereotype.Component;
+
+@Component("GuavaRateLimiter")
+@RequiredArgsConstructor
+public class GuavaRateLimiter implements RateLimiter {
+
+    private static final Map<String, Integer> requestCountPerIp = new ConcurrentHashMap<>();
+    private static final Map<String, LocalDateTime> requestTimeForIp = new ConcurrentHashMap<>();
+    private static final Map<String, LocalDateTime> blockedTimeForIp = new ConcurrentHashMap<>();
+
+    @Override
+    public void tryApiCall(String key, LimitRequestPerTime limitRequestPerTime, ProceedingJoinPoint joinPoint)
+            throws Throwable {
+
+        /*
+         TODO:
+          blockedTimeForIp에 ip 있는지 확인
+          있다면 (차단 당한 ip이거나 차단 풀린 ip의 요청)
+            현재 시간과 차이가 1시간 이내라면 (아직 차단 중)
+                예외 처리
+            현재 시간과 차이가 1시간 넘어섰다면 (차단 해제 됨)
+                blockedTimeForIp에서 ip 삭제
+                아래 로직 수행
+          없다면
+            현재 시간 확인
+            requestTimeForIP에 ip 있는지 확인
+            있다면
+                시간 차이 1분이내 인지 확인
+                    1분 이내라면 (1분 내에 같은 요청 들어온 상황)
+                        requestCountPerIp에 count++
+                        requestCountPerIp의 값이 50이라면
+                            blockedTimeForIp에 ip key 값 넣음
+                            예외 처리    
+                        requestCountPerIp의 값이 50이 아니라면
+                            문제 X
+                    1분 넘어선다면 (1분 지나서 요청 들어온 상황)
+                        현재 시간 넣기 & requestCountPerIp에 1 넣기
+            없다면
+                현재 시간 넣기 & requestCountPerIp에 1 넣기
+         */
+
+    }
+}
