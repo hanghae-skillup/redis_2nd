99620c6620e9484d1f3fde06599347a9e014e230
CJ-1998 <choijun0627@konkuk.ac.kr>
2025-04-06 19:24:00 +0900

    Feat : 예약 Rate Limit 위한 annotation 생성

---
+++ module_infrastructure/src/main/java/project/redis/ratelimiter/reserveratelimiter/LimitReservationPerTime.java
@@ -0,0 +1,26 @@
+package project.redis.ratelimiter.reserveratelimiter;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.concurrent.TimeUnit;
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LimitReservationPerTime {
+
+    long userId();
+
+    long screeningId();
+
+    /**
+     * 차단 시간
+     */
+    int blockTime() default 5;
+
+    /**
+     * 시간 단위
+     */
+    TimeUnit timeUnit() default TimeUnit.MINUTES;
+}
