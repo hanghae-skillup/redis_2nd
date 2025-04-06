f33c02e4cc108ddd4ccb06a3070ad814a5dc9ada
CJ-1998 <choijun0627@konkuk.ac.kr>
2025-04-06 12:30:08 +0900

    Refactor : reservation valid 클래스 생성
    ReservationService에서 수행하던 valid 로직 클래스로 추출
    ReservationValidator라는 클래스에서 수행하도록 이동

--- module_application/src/main/java/project/redis/reservation/service/ReservationService.java
+++ module_application/src/main/java/project/redis/reservation/service/ReservationService.java
@@ -12,6 +12,7 @@ import project.redis.reservation.Reservation;
 import project.redis.reservation.adapter.ReservationAdapter;
 import project.redis.reservation.dto.ReservationSeatsRequestDto;
 import project.redis.reservation.dto.ReservationSeatsResponseDto;
+import project.redis.reservation.validator.ReservationValidator;
 import project.redis.screening.Screening;
 import project.redis.screening.adapter.ScreeningAdapter;
 import project.redis.seat.Seat;
@@ -32,11 +33,12 @@ public class ReservationService {
     private final ReservationAdapter reservationAdapter;
     private final SeatAdapter seatAdapter;
     private final MessageService messageService;
+    private final ReservationValidator reservationValidator;
 
     @Transactional
     @DistributedLock(key = "seat-lock:#reservationSeatsRequestDto.userId")
     public ReservationSeatsResponseDto reserveSeats(ReservationSeatsRequestDto reservationSeatsRequestDto) {
-        valid(reservationSeatsRequestDto);
+        reservationValidator.valid(reservationSeatsRequestDto);
 
         User user = findUserByUserId(reservationSeatsRequestDto);
         Screening screening = findScreeningByScreeningId(reservationSeatsRequestDto);
@@ -49,35 +51,6 @@ public class ReservationService {
         List<Long> reservationsId = createReservations(reservationSeatsRequestDto, reservations, screening, user);
 
         return ReservationSeatsResponseDto.of(user.getUserId(), reservationsId);
-    }
-
-    private void valid(ReservationSeatsRequestDto requestDto) {
-        List<String> seatRows = requestDto.getSeatRows();
-        List<Integer> seatColumns = requestDto.getSeatColumns();
-
-        if (seatRows.isEmpty() || seatColumns.isEmpty()) {
-            throw new IllegalArgumentException("seatRows 또는 seatColumns가 비어 있습니다.");
-        }
-
-        if (seatRows.size() != seatColumns.size()) {
-            throw new IllegalArgumentException("seatRows와 seatColumns의 개수가 같지 않습니다.");
-        }
-
-        long seatRowCount = seatRows.stream().distinct().count();
-        if (seatRowCount > 1) {
-            throw new IllegalArgumentException("예약하려는 좌석들의 행이 이어 붙어 있는 형태가 아닙니다.");
-        }
-
-        long seatColumnCount = seatColumns.stream().distinct().count();
-        if (seatColumnCount != seatColumns.size()) {
-            throw new IllegalArgumentException("예약하려는 좌석의 열이 중복됩니다.");
-        }
-
-        for (int index = 1; index < seatColumns.size(); index++) {
-            if (seatColumns.get(index) != seatColumns.get(index - 1) + 1) {
-                throw new IllegalArgumentException("예약하려는 좌석의 열이 연속되는 형태가 아닙니다.");
-            }
-        }
     }
 
     private User findUserByUserId(ReservationSeatsRequestDto reservationSeatsRequestDto) {
