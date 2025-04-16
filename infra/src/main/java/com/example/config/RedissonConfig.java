95a13cbc6d45029119b469f81e2ad8595edd2d75
LAPTOP-K37MMFLI\stran <strangehoon2@gmail.com>
2025-04-16 19:44:36 +0900

    refactor : redissonconfiig @value 설정

--- infra/src/main/java/com/example/config/RedissonConfig.java
+++ infra/src/main/java/com/example/config/RedissonConfig.java
@@ -3,17 +3,25 @@ package com.example.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedissonConfig {
 
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
+        String redisUrl = String.format("redis://%s:%d", host, port);
         config.useSingleServer()
-                .setAddress("redis://127.0.0.1:6379");
+                .setAddress(redisUrl);
 
         return Redisson.create(config);
     }
